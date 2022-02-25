ab37e3a2add4cff078efa2156edcee1986ffa59a
Osama Ishaq <osama.ishaq@ibex.co>
2022-02-25 21:41:13 +0500

    Path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3003/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:3003/"
+        : "https://module-federation-add-to-cart.vercel.app/",
   },
 
   resolve: {
